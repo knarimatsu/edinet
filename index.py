05d589d2294f04c3c76ae7e0c939eb13ed47debb
knarimatsu <nariomasahisa@gmail.com>
2024-06-14 10:48:04 +0900

    feat: データ取得自動化ファイル作成

--- index.py
+++ index.py
@@ -1,80 +1,31 @@
-# import zipfile
-# import os
-# import chardet
-# from bs4 import BeautifulSoup
-
-# zip_file_path = './xbrl/Xbrl_Search_20240521_111841.zip'
-# exact_folder = './xbrl/extracted'
-
-# with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-#     zip_ref.extractall(exact_folder)
-
-# for root, dirs, files in os.walk(exact_folder):
-#     for file in files:
-#         if file.endswith('.xbrl'):
-#             xbrl_file_path = os.path.join(root, file)
-#             break
-
-# with open(xbrl_file_path, 'rb') as file:
-#     raw_data = file.read()
-
-# result = chardet.detect(raw_data)
-# encoding = result['encoding']
-
-# with open(xbrl_file_path, 'r', encoding=encoding) as file:
-#     xbrl_content = file.read()
-
-# soup = BeautifulSoup(xbrl_content, features='lxml')
-# elements = soup.find_all('jpcrp_cor:OperatingRevenue')
-
-# for element in elements:
-#     print(element.text)
-
-import zipfile
 import os
-from bs4 import BeautifulSoup
-import chardet
+from time import sleep
+from get_docid import get_id_list
+from xbrl_get import download_xbrl_file
+from xbrl_read import detect_xbrl, get_xbrl_data
 
-# ZIPファイルのパス
-zip_file_path = './xbrl/Xbrl_Search_20240521_111841.zip'
-extract_folder = './xbrl/extracted'
 
-# ZIPファイルを解凍
-with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-    zip_ref.extractall(extract_folder)
+# ['S100TLCL', 'S100TLBC']
 
-# 解凍されたファイルのパス（解凍されたフォルダの中のXBRLファイルを探す）
-xbrl_file_path = None
-for root, dirs, files in os.walk(extract_folder):
-    for file in files:
-        if file.endswith('.xbrl'):
-            xbrl_file_path = os.path.join(root, file)
-            break
+print("昨日発行された一般企業の有価証券報告書のIDを取得します")
+docment_id_list = get_id_list()
+print(docment_id_list)
+print("---------------------------")
+print("取得完了しました")
+print("---------------------------")
 
-# XBRLファイルが見つからない場合のエラーハンドリング
-if xbrl_file_path is None:
-    raise FileNotFoundError("XBRLファイルが見つかりませんでした。")
+print("取得したIDからの有価証券報告書をダウンロードします")
+for doc_id in docment_id_list:
+    download_xbrl_file(doc_id)
+    sleep(2)
+print("---------------------------")
+print("すべてのダウンロード完了しました")
+print("---------------------------")
 
-# XBRLファイルのバイナリデータを読み込む
-with open(xbrl_file_path, 'rb') as file:
-    raw_data = file.read()
+for doc_id in docment_id_list:
+    print("---------------------------")
+    get_xbrl_data(doc_id)
+    os.chdir("..")
+    os.chdir("..")
 
-# エンコーディングを検出
-result = chardet.detect(raw_data)
-encoding = result['encoding']
-
-# 検出されたエンコーディングを使用してファイルを読み込む
-with open(xbrl_file_path, 'r', encoding=encoding) as file:
-    xbrl_content = file.read()
-
-# BeautifulSoupでlxmlパーサーを使用して解析
-soup = BeautifulSoup(xbrl_content, features="xml")
-
-# 例：特定のタグを取得
-elements_revenue = soup.find_all('jpcrp_cor:OperatingRevenue')
-elements_profit = soup.find_all('jpcrp_cor:NetIncome')
-for element in elements_revenue:
-    print(element.text)
-
-for element in elements_profit:
-    print(element.text)
+print("---------------------------")
